46fcaab7cdb90c3dc46690ecc9653b91efeb9c77
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 15:49:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
-array.join(",")
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
